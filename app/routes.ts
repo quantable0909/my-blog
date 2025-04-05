935bfd2b61268f6ca38c3a5bd37c90e556ab7454
quantable0909 <quantaplayground@gmail.com>
2025-04-05 17:14:01 +0900

    feat: add new pages for about, all posts, travel, lifestyle, destination, and post details with routing updates

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,17 @@
-import { type RouteConfig, index } from '@react-router/dev/routes';
+import {
+  type RouteConfig,
+  index,
+  prefix,
+  route,
+} from '@react-router/dev/routes';
 
 export default [
   // 홈페이지 인덱스 경로 수정
   index('index.tsx'),
+  route('/about', 'pages/about.tsx'),
+  ...prefix('post', [route('/:id', 'pages/post.tsx')]),
+  route('/all-post', 'pages/all-post.tsx'),
+  route('/travel', 'pages/travel.tsx'),
+  route('/lifestyle', 'pages/lifestyle.tsx'),
+  route('/destination', 'pages/destination.tsx'),
 ] satisfies RouteConfig;
